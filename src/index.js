b0ac24b3243f2263c0fee80009b6a5dc0948bef8
grigor-aramyan <grigori.aramyan@gmail.com>
2020-01-26 21:51:10 +0400

    add components folder and App component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Components
+import App from './components/App';
+
 ReactDOM.render(
     <Router>
         <Route path='/'>
             <Switch>
                 <Route exact path='/' component={App} />
-                <Route path='/test' component={TestView} />
                 <Route path='/*' render={ () => {
                     return(
                         <div>
