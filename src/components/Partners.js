cee0200bb14f472f35ac0cf00e0e07845484fcd1
Tigran Manasyan <tigranmanasyan7@gmail.com>
2020-02-09 23:08:03 +0400

    component Partners

---
+++ src/components/Partners.js
@@ -0,0 +1,23 @@
+import React from "react";
+ export default class extends React.Component {
+     render(){
+        return(
+            <div className = "container-fluid new-partners">
+                <h1>New partners</h1>
+                <span></span>
+                <div className = "row">
+                    <div className = "col-md-4">
+                        1111
+                    </div>
+                    <div className = "col-md-4">
+                        1111
+                    </div>
+                    <div className = "col-md-4">
+                        1111
+                    </div>
+   
+                </div>
+            </div>
+        )
+     }
+ }
