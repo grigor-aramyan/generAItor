47e732b6702e191eb0cf52d02089e1fcb57aa279
Garik1113 <58729921+Garik1113@users.noreply.github.com>
2020-02-09 21:24:56 +0400

    header, Service, Footer components

---
+++ src/components/Pricing/Pricing.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Pricing extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="image-wrapper">
+          <img
+            className="img-responsive"
+            src="images/pricing_page_image.png"
+            alt="Chania"
+          />
+        </div>
+      </div>
+    );
+  }
+}
