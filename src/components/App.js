b2efce95e827a062012ac8a1ad86778a699935a8
Diana Tumanyan <dintumik@gmail.com>
2020-02-09 19:17:05 +0400

    added Info.js component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 // import Main from './Main';
+// import Info from './Info';
 
 // actions
 import {
