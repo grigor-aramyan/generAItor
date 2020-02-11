19e402e695cd0d7e975c7f7b7132a1b90b1c89b3
Diana Tumanyan <dintumik@gmail.com>
2020-02-11 21:32:29 +0400

    added AboutUsPage composition

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,34 +3,41 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <div className="col-lg-12 col-md-12">
-          <div className="header-wrapper">
-            <div className="header-logo-wrapper">
-              <img src="images/logo_official.png" alt="logo_official" />
+      <div className="container-fluid header">
+          <div className = "row">
+            <div className = "col-md-2 a">
+              <div className="header-logo-wrapper">
+                <a href = "#">
+                  <img src="images/logo_official.png" alt="logo_official" />
+                </a>
+              </div>
             </div>
-            <div className="header-profile-logo-wrapper">
-              <img src="images/noun_profile_logo.png" alt="profile_logo" />
-            </div>
-            <div className="heaader-burger">
-              <img
-                src="images/Rectangle.png"
-                alt="rectangle"
-                className="header-rectangle1"
-              />
-              <img
-                src="images/Rectangle.png"
-                alt="rectangle"
-                className="header-rectangle2"
-              />
-              <img
-                src="images/Rectangle.png"
-                alt="rectangle"
-                className="header-rectangle3"
-              />
+            <div className = "offset-md-8"></div>
+            <div className = "col-md-2">
+              <div className="header-wrapper d-flex justify-content-end">
+                <div className="header-profile-logo-wrapper">
+                  <img src="images/noun_profile_logo.png" alt="profile_logo" />
+                </div>
+                <div className="heaader-burger d-flex flex-column">
+                  <img
+                    src="images/Rectangle.png"
+                    alt="rectangle"
+                    className="header-rectangle1"
+                  />
+                  <img
+                    src="images/Rectangle.png"
+                    alt="rectangle"
+                    className="header-rectangle2"
+                  />
+                  <img
+                    src="images/Rectangle.png"
+                    alt="rectangle"
+                    className="header-rectangle3"
+                  />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
       </div>
     );
   }
