0d6d455e7a53a0fb3678939568d38c75ef55e389
grigor-aramyan <grigori.aramyan@gmail.com>
2020-03-22 21:08:00 +0400

    add error reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import counterReducer from './counterReducer';
 import userReducer from './userReducer';
+import errorReducer from './errorReducer';
 
 export default combineReducers({
     counter: counterReducer,
+    error: errorReducer,
     user: userReducer
 });
