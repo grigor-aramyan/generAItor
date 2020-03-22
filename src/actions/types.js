dc8f3f6fb44935c1f47564c60d6ab80d3d51e16d
grigor-aramyan <grigori.aramyan@gmail.com>
2020-03-22 20:49:51 +0400

    add error redux action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,8 +3,13 @@ export const DECREMENT = 'DECREMENT';
 export const INCREMENT_BY_NUMBER = 'INCREMENT_BY_NUMBER';
 export const DECREMENT_BY_NUMBER = 'DECREMENT_BY_NUMBER';
 
+// Error types
+// Error types
+export const GET_ERRORS = 'GET_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 
 
+// Register/login types
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_IN_FAILED = 'SIGN_IN_FAILED';
 export const SIGN_UP = 'SIGN_UP';
