eeeaf6ac90c9beb0c33902b28c420354a5e54980
grigor-aramyan <grigori.aramyan@gmail.com>
2020-03-22 19:46:31 +0400

    add register/login redux action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,3 +2,14 @@ export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 export const INCREMENT_BY_NUMBER = 'INCREMENT_BY_NUMBER';
 export const DECREMENT_BY_NUMBER = 'DECREMENT_BY_NUMBER';
+
+
+
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_IN_FAILED = 'SIGN_IN_FAILED';
+export const SIGN_UP = 'SIGN_UP';
+export const SIGN_UP_FAILED = 'SIGN_UP_FAILED';
+export const SIGN_OUT = 'SIGN_OUT';
+export const SIGN_OUT_FAILED = 'SIGN_OUT_FAILED';
+export const GET_PROFILE_DATA = 'GET_PROFILE_DATA';
+export const GET_PROFILE_DATA_FAILED = 'GET_PROFILE_DATA_FAILED';
