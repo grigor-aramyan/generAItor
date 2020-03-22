3ac667941ab65a11a4c7cafcb0d840c06bc6dcf0
grigor-aramyan <grigori.aramyan@gmail.com>
2020-03-22 20:41:52 +0400

    add initial user reducer

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,67 @@
+import {
+    SIGN_IN,
+    SIGN_IN_FAILED,
+    SIGN_OUT,
+    SIGN_OUT_FAILED,
+    SIGN_UP,
+    SIGN_UP_FAILED,
+    GET_PROFILE_DATA,
+    GET_PROFILE_DATA_FAILED
+} from '../actions/types';
+
+const initialState = {
+    token: '',
+    isAuthenticated: false,
+    user: null,
+    organization: null,
+    ideaGeneraitor: null
+}
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case SIGN_UP:
+        case SIGN_IN:
+            localStorage.setItem('innojwt', action.token);
+
+            return {
+                ...state,
+                isAuthenticated: true,
+                token: action.token
+            };
+        case SIGN_UP_FAILED:
+        case SIGN_IN_FAILED:
+        case SIGN_OUT:
+            localStorage.removeItem('innojwt');
+
+            return {
+                ...state,
+                token: '',
+                isAuthenticated: false,
+                user: null,
+                organization: null,
+                ideaGeneraitor: null
+            };
+        case SIGN_OUT_FAILED:
+            return state;
+        case GET_PROFILE_DATA:
+            profileType = action.payload.user.profile_type;
+
+            if (profileType == 'Organization') {
+                return {
+                    ...state,
+                    user: action.payload.user,
+                    organization: action.payload.profile
+                };
+            } else if (profileType == 'IdeaGeneraitor') {
+                return {
+                    ...state,
+                    user: action.payload.user,
+                    ideaGeneraitor: action.payload.profile
+                };
+            }
+        case GET_PROFILE_DATA_FAILED:
+            return state;
+        default:
+            return state;
+    }
+}
